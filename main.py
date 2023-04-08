98497eda1398f282a8dc7616040061943529c8e3
Aizen2001 <kalinakuzandreygalina@gmail.com>
2023-04-09 03:17:11 +0500

    commit message

--- main.py
+++ main.py
@@ -1,27 +1,117 @@
-from typing import Optional
-from fastapi import FastAPI
-from model.dbHandler import match_exact, match_like
+from typing import List , Optional
 
-app = Flask(__name__)
+from fastapi import FastAPI , Query
 
+from fastapi . encoders import jsonable_encoder
 
-@app.get("/")
-def index():
-    """
+from model . dbHandler import match_exact , match_like
+
+app = FastAPI ()
+
+ 
+
+ 
+
+@ app . get ( '/' )
+
+def   index ():
+
+     '''
+
     DEFAULT ROUTE
+
     This method will
+
     1. Provide usage instructions formatted as JSON
-    """
-    return "TODO"
+
+    '''
+
+     response   =  { 'usage' :  '/dict?=<word>' }
+
+     return   jsonable_encoder ( response )
 
 
-@app.get("/dict")
-def dictionary():
-    """
-    DEFAULT ROUTE
+@ app . get ( '/dict' )
+
+def   dictionary ( words :  List [ str ]  =   Query ( None )):
+
+     '''
+
+    DICTIONARY ROUTE
+
     This method will
+
     1. Accept a word from the request
+
     2. Try to find an exact match, and return it if found
+
     3. If not found, find all approximate matches and return
-    """
-    return "TODO"
+
+    '''
+
+     if   not   words :
+
+         response   =  {
+
+             'status' :  'error' , 
+
+             'word' :  words , 
+
+             'data' :  'word not found'
+
+            }
+
+         return   jsonable_encoder ( response )
+     response   =  { 'words' : []}
+
+     for   word   in   words :
+
+         # Try to find an exact match
+
+         definitions   =   match_exact ( word )
+
+         if   definitions :
+
+             response [ 'words' ]. append ({
+
+                 'status' :  'success' , 
+
+                 'word' :  word , 
+
+                 'data' :  definitions
+
+                })
+
+         else :
+
+             # Try to find an approximate match
+
+             definitions   =   match_like ( word )
+
+             if   definitions :
+
+                 response [ 'words' ]. append ({
+
+                     'status' :  'partial' , 
+
+                     'word' :  word , 
+
+                     'data' :  definitions
+
+                    })
+
+             else :
+
+                 response [ words ]. append ({
+
+                     'status' :  'error' , 
+
+                     'word' :  word , 
+
+                     'data' :  'word not found'
+
+                    })
+
+ 
+
+     return   jsonable_encoder ( response )
